efab4a9639fe1e6979e6cf2c2e60980e91fc6451
Spekcs <speccer.fim@gmail.com>
2024-08-20 18:36:42 +0300

    Added HTMLNode and tests

---
+++ src/test_htmlnode.py
@@ -0,0 +1,16 @@
+import unittest
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+    def test_props_to_html(self):
+        node = HTMLNode(props={"href":"url", "target":"_blank"})
+        self.assertEqual(node.props_to_html(), " href=\"url\" target=\"_blank\"")
+
+    def test_props_to_html_empty(self):
+        node = HTMLNode()
+        self.assertEqual(node.props_to_html(), "")
+
+    def test_props_to_html(self):
+        node = HTMLNode(props = {"hi": "some_url"})
+        self.assertEqual(node.props_to_html(), " hi=\"some_url\"")
+        
