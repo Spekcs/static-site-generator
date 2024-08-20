de5a4dac4a6d2f32af0ce25501d76a33c1d634a9
Spekcs <speccer.fim@gmail.com>
2024-08-20 18:55:16 +0300

    Added leafnode

---
+++ src/test_leafnode.py
@@ -0,0 +1,19 @@
+import unittest
+from leafnode import LeafNode
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html(self):
+        node = LeafNode("Hi", "a", {"href":"url.com"})
+        self.assertEqual(node.to_html(), "<a href=\"url.com\">Hi<a>")
+    
+    def test_to_html_empty(self):
+        node = LeafNode(None)
+        self.assertRaises(ValueError, node.to_html)
+
+    def test_to_html_no_props(self):
+        node = LeafNode("Hi", "p")
+        self.assertEqual(node.to_html(), "<p>Hi<p>")
+
+    def test_to_html_no_tag(self):
+        node = LeafNode("Hi")
+        self.assertEqual(node.to_html(), "Hi")
